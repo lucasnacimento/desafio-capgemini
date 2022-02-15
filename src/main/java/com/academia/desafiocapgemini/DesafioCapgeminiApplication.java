126f46d20ee6e3feacab628f4f8296093261c7e4
Lucas Nascimento Lopes <85919181+lucasnacimento@users.noreply.github.com>
2022-02-15 14:15:06 -0300

    commit inicial

---
+++ src/main/java/com/academia/desafiocapgemini/DesafioCapgeminiApplication.java
@@ -0,0 +1,13 @@
+package com.academia.desafiocapgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCapgeminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academia/desafiocapgemini/DesafioCapgeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.desafiocapgemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCapgeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
